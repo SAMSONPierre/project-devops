e1418970c0f63fe78e4447cbd9be0072c5db21cb
SAMSON Pierre <102805875+SAMSONPierre@users.noreply.github.com>
2024-12-09 14:01:37 +0100

    Update main.go

--- webapi/main.go
+++ webapi/main.go
@@ -21,6 +21,7 @@ func whoAmI(response http.ResponseWriter, r *http.Request) {
 	who := []whoami{
 		whoami{Name: "Efrei Paris",
 			Title: "DevOps and Continous Deployment",
+		       	Desc: "Team no flop",
 			State: "FR",
 		},
 	}
